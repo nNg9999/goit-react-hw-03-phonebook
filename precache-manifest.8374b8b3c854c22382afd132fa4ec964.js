3f48fd192cd02815dd59a47936dd765b0bafe538
nNg9999 <n_n_g_@ukr.net>
2020-06-16 17:44:08 +0300

    Updates

---
+++ precache-manifest.8374b8b3c854c22382afd132fa4ec964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe3caa5d09f795955745342bd11055d",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "c3d635301c9195d582de",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.85ca5d7d.chunk.css"
+  },
+  {
+    "revision": "2bdd9de46753645e5297",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b47e2f51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b47e2f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d635301c9195d582de",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.61229cf4.chunk.js"
+  },
+  {
+    "revision": "cb8f4a199435ab9dcb6f",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.b69d4358.js"
+  }
+]);
